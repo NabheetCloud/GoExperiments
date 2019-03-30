1dea130a8a1c45610c7bf1fb6c6a7e497c14a909
Nabheet Madan <nabheet.cloud@gmail.com>
2019-03-30 13:35:00 +0530

    #GoDay2

--- 02_vars/main.go
+++ 02_vars/main.go
@@ -3,12 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	var name  string = "Nabheet"
+	// different ways for variable declarations
+	var name string = "Nabheet"
 	//short hand
 	name2 := "Nabheet 2"
 	size := 1.30
+	lastname, lastname2 := "Madan", "Madan2"
 	var age int = 37
-	fmt.Println(name,name2, age)
-	fmt.Printf("%T\n",size)
-	fmt.Printf("%T\n",age)
+	fmt.Println(name, name2, age)
+	fmt.Println(lastname, lastname2)
+	fmt.Printf("%T\n", size)
+	fmt.Printf("%T\n", age)
 }
