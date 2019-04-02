2d5a1d113df11dd5e73fc015800c322ba663331a
Nabheet Madan <nabheet.cloud@gmail.com>
2019-04-02 07:44:45 +0530

    #GoDay4

--- 06_conditionals/main.go
+++ 06_conditionals/main.go
@@ -3,9 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	x := 5
-	y := 6
-	if x < y {
-		fmt.Printf("%d is less than %d\n", x, y)
+	x := 9
+	y := 7
+	if x <= y {
+		fmt.Printf("%d is less than or equal %d\n", x, y)
+	} else {
+		fmt.Printf("%d is less than %d\n", y, x)
+	}
+
+	color := "red"
+	if color == "red" {
+		fmt.Println("Color is red")
+	} else if color == "blue" {
+		fmt.Println("color is blue")
+	} else {
+		fmt.Println("color is not blue nor red")
+	}
+	switch color {
+	case "red":
+		fmt.Println("Color is red")
+	case "blue":
+		fmt.Println("Color is blue")
+	default:
+		fmt.Println("color is not blue nor red")
 	}
 }
