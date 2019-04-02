cc7ae6cdcf2a0feb12db5085a47c960980f69068
Nabheet Madan <nabheet.cloud@gmail.com>
2019-04-02 08:49:34 +0530

    #GoDay4

--- 09_range/main.go
+++ 09_range/main.go
@@ -3,5 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	ids := []int{1, 2, 321, 21, 11}
+	for i, id := range ids {
+		fmt.Printf("%d - ID: %d\n", i, id)
+	}
+
+	for id := range ids {
+		fmt.Printf(" ID: %d\n", id)
+	}
+	for _, id := range ids {
+		fmt.Printf(" ID: %d\n", id)
+	}
+
+	sum := 0
+	for _, id := range ids {
+		sum += id
+	}
+	fmt.Println("Sum", sum)
+
+	email := map[string]string{
+		"nabh": "test@test,com", "nabh1": "test@com"}
+	for k, v := range email {
+		fmt.Printf("%s: %s\n", k, v)
+	}
+
 }
