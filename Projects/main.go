6c9833ef37aa2c7cb6996d4d50d5893c2274ed18
Nabheet Madan <nabheet.cloud@gmail.com>
2019-04-03 07:10:29 +0530

    #GoDay5

--- Projects/main.go
+++ Projects/main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
+type SitemapIndex struct {
+	Locations []Location `xml:"sitemap"`
+}
+
+type Location struct {
+	Loc string `xml:"loc"`
+}
+
+func (l Location) String() string {
+	return fmt.Sprintf(l.Loc)
+}
 func main() {
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
-	stringbody := string(bytes)
-	fmt.Println(stringbody)
 	resp.Body.Close()
+	var s SitemapIndex
+	xml.Unmarshal(bytes, &s)
+	for _, Location := range s.Locations {
+		fmt.Printf("\n%s", Location)
+	}
 }
