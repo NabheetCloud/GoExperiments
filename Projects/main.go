3867570193846c759a0ac4a03ca95800d2f79c2e
Nabheet Madan <nabheet.cloud@gmail.com>
2019-04-03 06:48:18 +0530

    #GoDay5

---
+++ Projects/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	stringbody := string(bytes)
+	fmt.Println(stringbody)
+	resp.Body.Close()
+}
